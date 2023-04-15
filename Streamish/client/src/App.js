204c865984dc5d34fc39130ccbe48c5b6e77c3de
Will Hedges <wshedges@outlook.com>
2023-04-15 15:20:17 -0500

    added 'styles' dir, video form, styled components * all .css files now in styles dir * styled form, search components w/ reactstrap * search bar + video form working * video form defaults to user id 0 - will look at

--- Streamish/client/src/App.js
+++ Streamish/client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import "./App.css";
+import "./styles/App.css";
+import VideoForm from "./components/VideoForm";
 import VideoList from "./components/VideoList";
 
 function App() {
   return (
     <div className="App">
+      <VideoForm />
       <VideoList />
     </div>
   );
--- Streamish/client/src/components/VideoList.js
+++ Streamish/client/src/components/VideoList.js
@@ -3,6 +3,10 @@ import React, { useEffect, useState } from "react";
 import Video from "./Video";
 import { getAllVideos, searchVideos } from "../modules/videoManager";
 
+import { Button, Input, InputGroup, InputGroupText } from "reactstrap";
+
+import "../styles/VideoList.css";
+
 const VideoList = () => {
   const [videos, setVideos] = useState([]);
   const [searchTerms, setSearchTerms] = useState("");
@@ -26,31 +30,45 @@ const VideoList = () => {
 
   return (
     <div className="container">
-      <label htmlFor="search-bar">Search:</label>
-      <input
-        name="search-bar"
-        type="text"
-        value={searchTerms}
-        onChange={(evt) => {
-          setSearchTerms(evt.target.value);
-        }}
-      />
-      <label htmlFor="sort-desc-checkbox">Sort Descending:</label>
-      <input
-        type="checkbox"
-        name="sort-desc-checkbox"
-        value="desc"
-        onChange={(evt) => {
-          setSortDesc(evt.target.checked);
-        }}
-      />
-      <button
-        onClick={() =>
-          searchVideos(searchTerms, sortDesc).then((res) => setVideos(res))
-        }
-      >
-        Go!
-      </button>
+      <div className="search__container">
+        <InputGroup>
+          <InputGroupText>Search</InputGroupText>
+          <Input
+            name="search-bar"
+            type="text"
+            value={searchTerms}
+            onChange={(evt) => {
+              setSearchTerms(evt.target.value);
+            }}
+          />
+          <InputGroupText>
+            Descending?
+            <Input
+              type="checkbox"
+              id="sort-descending__checkbox"
+              onChange={(evt) => {
+                setSortDesc(evt.target.checked);
+              }}
+            />
+          </InputGroupText>
+        </InputGroup>
+        {searchTerms ? (
+          <Button
+            className="search__button"
+            color="primary"
+            onClick={() =>
+              searchVideos(searchTerms, sortDesc).then((res) => setVideos(res))
+            }
+          >
+            Go!
+          </Button>
+        ) : (
+          <Button disabled className="search__button">
+            Go!
+          </Button>
+        )}
+      </div>
+
       <div className="row justify-content-center">
         {videos.map((video) => (
           <Video video={video} key={video.id} />
