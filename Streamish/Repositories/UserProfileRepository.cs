cbc33da900b4891c1581c813385988ff80deb90a
Will Hedges <wshedges@outlook.com>
2023-04-18 14:48:38 -0500

    Register new users with Firebase + db in one step * client gets FirebaseUserId on register * added to INSERT SQL query * added DateTime.Now to new user obj * tested + working

--- Streamish/Repositories/UserProfileRepository.cs
+++ Streamish/Repositories/UserProfileRepository.cs
@@ -177,10 +177,11 @@ namespace Streamish.Repositories
                 {
                     cmd.CommandText =
                         @"
-                        INSERT INTO UserProfile (Name, Email, ImageUrl, DateCreated)
+                        INSERT INTO UserProfile (FirebaseUserId, Name, Email, ImageUrl, DateCreated)
                         OUTPUT INSERTED.ID
-                        VALUES (@Name, @Email, @ImageUrl, @DateCreated)";
+                        VALUES (@FirebaseUserId, @Name, @Email, @ImageUrl, @DateCreated)";
 
+                    DbUtils.AddParameter(cmd, "@FirebaseUserId", userProfile.FirebaseUserId);
                     DbUtils.AddParameter(cmd, "@Name", userProfile.Name);
                     DbUtils.AddParameter(cmd, "@Email", userProfile.Email);
                     DbUtils.AddParameter(cmd, "@ImageUrl", userProfile.ImageUrl);
